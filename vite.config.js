00799abcf973c6c657942f7af1b747e60d802046
MedusaOnMe <benhemple3@gmail.com>
2025-06-05 21:16:06 +0100

    hosts

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: [
+      '3adb6593-930d-4628-b86e-d0e5b32dd8ca-00-1hlpyfamqto0x.kirk.replit.dev',
+      'tekken-medusaon.replit.app',
+      'billfighter.fun'
+    ]
   },
   build: {
     outDir: 'dist',
